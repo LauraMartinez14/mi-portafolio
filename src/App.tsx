f8a2c50ce88735b3ee35fc5a8884a8266f22a19c
LauraMartinez14 <martinez.laura.andrea14@gmail.com>
2025-11-04 02:50:20 -0400

    fix: importancion

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Descripcion from './Componets/info'
-import Proyects from './Componets/proyects'
 import Card from './Componets/card'
 
 function App() {
